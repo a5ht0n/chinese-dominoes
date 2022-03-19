9e15ee9633bb797d5dc52d0458051f4509601240
Ashton Marsh <ashton.marsh@lexisnexis.co.za>
2022-03-19 17:39:19 +0200

    validation rule test

--- src/components/ContestantSignUp.vue
+++ src/components/ContestantSignUp.vue
@@ -1,62 +1,47 @@
 <template>
- <div>HELLO!</div>
+    <v-container>
+        <v-text-field
+            label="Player 1*"            
+            v-model="players[0]"
+            :rules="[players[0] !== undefined, players[0] !== '']"
+            autofocus
+            class="danger"
+        ></v-text-field>
+        <v-text-field
+            label="Player 2*"
+            v-model="players[1]"
+            :rules="[players[1] !== undefined, players[1] !== '']"
+        ></v-text-field>
+        <v-text-field
+            label="Player 3*"
+            v-model="players[2]"
+            :rules="[players[2] !== undefined, players[2] !== '']"
+        ></v-text-field>
+        <v-text-field
+            label="Player 4*"
+            v-model="players[3]"
+            :rules="[players[3] !== undefined, players[3] !== '']"
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-btn
+            block
+            color="accent"
+            elevation="2"
+            v-on:click="showAlert()"
+        >Start</v-btn>
+    </v-container>
 </template>
 
 <script>
   export default {
     name: 'ContestantSignUp',
-
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+        players:[]
     }),
+    methods:{
+        showAlert: function(){
+            alert(this.players[0]);
+        }
+    }
   }
 </script>
